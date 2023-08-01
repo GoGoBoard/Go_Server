fecbf97e94ba96663deec6525454471dfc35deda
Kim mingyu <minq0302@naver.com>
2023-08-01 20:06:51 +0900

    게시글 테이블 복합키 삭제

--- src/main/java/Go/board/entity/Post.java
+++ src/main/java/Go/board/entity/Post.java
@@ -7,12 +7,11 @@ import javax.persistence.*;
 @Entity
 @Data
 @Table(name = "post")
-@IdClass(PostId.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int postId;
-    @Id
+
     private int memberId;
     private String title;
     private String content;
