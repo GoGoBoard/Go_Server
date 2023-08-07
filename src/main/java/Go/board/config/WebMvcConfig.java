ba95c296392de39d4602d25474a085e18ecc5702
Kim mingyu <minq0302@naver.com>
2023-08-07 21:23:41 +0900

    fix: Change allowedOriginPatterns CORS

--- src/main/java/Go/board/config/WebMvcConfig.java
+++ src/main/java/Go/board/config/WebMvcConfig.java
@@ -12,8 +12,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
-                .allowedOrigins("*")
+                // Origin이 http:localhost:3000에
+                .allowedOriginPatterns("*")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
