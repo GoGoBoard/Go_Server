137b9fb30c9d905300f9c3a0e5e02286acf3f529
Kim mingyu <minq0302@naver.com>
2023-08-14 18:22:14 +0900

    fix: 비밀번호 비교 로직 변경

--- src/main/java/Go/board/dto/PostDTO.java
+++ src/main/java/Go/board/dto/PostDTO.java
@@ -13,10 +13,4 @@ public class PostDTO {
     private String content;
     private String writeTime;
 
-    // entity는 기본 생성자를 반드시 생성해야 하지만, DTO는 그러지 않아도 괜찮다.
-
-    // DB는 Entity로 반환하므로, DTO로 타입을 변환해야한다.
-    // Post post = **Post entity**에 있는 정보를 PostDTO에 맵핑하는 중
-    // 실제 화면에서 사용할 것만 post entity에서 가져오면 된다
-
 }
---
+++ src/main/java/Go/board/repository/FileRepository.java
@@ -0,0 +1,7 @@
+package Go.board.repository;
+
+import Go.board.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+}
