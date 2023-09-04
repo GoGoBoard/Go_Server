a884df5f754f8724e1efa8aba1c6e1a557bf8278
jueun oh <jueun1025@naver.com>
2023-09-04 17:46:20 +0900

    [fix] file

--- src/main/java/Go/board/controller/ArticleController.java
+++ src/main/java/Go/board/controller/ArticleController.java
@@ -37,14 +37,14 @@ public class ArticleController {
     public ResponseEntity save(
             @RequestPart("title") String title,
             @RequestPart("content") String content,
-            @RequestPart("file") List<MultipartFile> files,
+            @RequestPart("file") List<MultipartFile> file,
             HttpServletRequest request
     ) {
         try {
             //todo XSS
             HttpSession session = request.getSession(false);
             int memberId = (int) session.getAttribute("memberId");
-            ArticleSaveDTO article = new ArticleSaveDTO(title, content, files);
+            ArticleSaveDTO article = new ArticleSaveDTO(title, content, file);
             articleService.save(article, memberId);
             return ResponseEntity.ok("저장 성공");
         } catch (Exception e) {
